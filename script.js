b379b8323cdf4041db7d18d0cd0da0f7a68e754b
Sindhu-18-2005 <sindhurabathini97@gmail.com>
2026-04-29 14:45:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+let currentIndex = 0;
+let images = document.querySelectorAll(".image");
+
+
+function openLightbox(img) {
+    
+  document.getElementById("lightbox").style.display = "flex";
+  document.getElementById("lightbox-img").src = img.src;
+  currentIndex = Array.from(images).indexOf(img);
+}
+
+function closeLightbox() {
+  document.getElementById("lightbox").style.display = "none";
+}
+
+function nextImage() {
+    
+  currentIndex = (currentIndex + 1) % images.length;
+  document.getElementById("lightbox-img").src = images[currentIndex].src;
+}
+
+function prevImage() {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    document.getElementById("lightbox-img").src = images[currentIndex].src;
+}
+   
+/* Filter Function */
+function filterImages(category) {
+  images.forEach(img => {
+    if (category === "all" || img.classList.contains(category)) {
+      img.style.display = "block";
+    } else {
+      img.style.display = "none";
+    }
+  });
+}
